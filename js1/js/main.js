aab85768ed20fd349b434bbe27b8e0bed41108ed
Jan Ponichterj <j.ponichter@wp.pj>
2019-06-10 01:35:11 +0200

    To-do basic structure

---
+++ js1/js/main.js
@@ -0,0 +1,21 @@
+﻿
+function point() {
+    let valueOfInput = document.getElementById("input").value;
+    let path = document.getElementById('list');
+    path.innerHTML += '<div class="pure-u-20-24 to-do">' + valueOfInput + '</div>';
+    document.getElementById("input").value = '';
+}
+
+document.getElementById('btn').addEventListener('click', point);
+document.getElementById('input').addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        point()
+    }
+});
+
+function setMaxLength() {
+    var input = document.getElementById("input");
+    input.maxLength = 28;
+}
+
+document.getElementById('input').addEventListener('click', setMaxLength);
