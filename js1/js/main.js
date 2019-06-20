876d28838070b82fe5bb77ca30fa70d3297d2d39
Jan Ponichterj <j.ponichter@wp.pj>
2019-06-20 21:17:54 +0200

    Button fixed, print button added(wip).

--- js1/js/main.js
+++ js1/js/main.js
@@ -2,9 +2,12 @@
 function point() {
     let valueOfInput = document.getElementById("input").value;
     let path = document.getElementById('list');
-    path.innerHTML += '<li id="div' + '\" class="to-do">' + valueOfInput +
-    '<button class="delete" onclick=\"return this.parentNode.remove();\"><i class=\"fas fa-window-close fa-2x\"></i></button></li>';
-    document.getElementById("input").value = '';
+
+    if (valueOfInput.length !== 0) {
+        path.innerHTML += '<li id="div' + '\" class="to-do">' + valueOfInput +
+            '<button class="delete" onclick=\"return this.parentNode.remove();\"><i class=\"fas fa-window-close fa-2x\"></i></button></li>';
+        document.getElementById("input").value = '';
+    } 
 }
 
 document.getElementById('btn').addEventListener('click', point);
@@ -16,8 +19,25 @@ document.getElementById('input').addEventListener('keydown', function (e) {
 
 function setMaxLength() {
     var input = document.getElementById("input");
-    input.maxLength = 28;
+    input.maxLength = 22;
 }
 
 document.getElementById('input').addEventListener('click', setMaxLength);
 
+function printPage() {
+    var content = document.getElementById('list').innerHTML;
+    var mywindow = window.open('', 'Print', 'height=600,width=800');
+
+    mywindow.document.write('<html><head><title>Print</title>');
+    mywindow.document.write('</head><body >');
+    mywindow.document.write(content);
+    mywindow.document.write('</body></html>');
+
+    mywindow.document.close();
+    mywindow.focus()
+    mywindow.print();
+    mywindow.close();
+    return true;
+}
+
+document.getElementById('button-print').addEventListener('click', printPage);
